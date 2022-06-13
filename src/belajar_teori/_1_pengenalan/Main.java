a2568f5d4c097ff09b09ebb9e7a7e61d5e6474dd
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2022-06-13 19:30:25 +0700

    add: introduction

---
+++ src/belajar_teori/_1_pengenalan/Main.java
@@ -0,0 +1,14 @@
+// Folder
+package belajar_teori._1_pengenalan;
+
+// class diisi sesuai Nama File
+class Main {
+
+    // method utama dimana semua kode akan berjalan di dalam public static void main
+    public static void main(String[] args) {
+
+        // Sebagai perintah untuk mencetak sebuah teks
+        System.out.println("Hello World!");
+
+    }
+}
